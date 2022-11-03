d1882100b4d007eae3bd0f83c7cc85bd32ae2c97
pandasAtHome <2197884129@qq.com>
2022-11-03 11:26:20 +0800

    修改：demo 模板

--- src/main/java/leetcode/sub/Demo01.java
+++ src/main/java/leetcode/sub/Demo01.java
@@ -1,4 +1,10 @@
 package leetcode.sub;
 
 public class Demo01 {
+    public static void main(String[] args) {
+        Solution solution = new Demo01().new Solution();
+    }
+
+    class Solution {
+    }
 }
